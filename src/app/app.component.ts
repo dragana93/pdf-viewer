60433f0aeda250ca9443911ce650ce8b8cf78cc9
draganasavanovic <dragana.savanovic@wayseven.com>
2020-02-03 11:17:32 +0100

    created simple pdf viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { SwalComponent } from "@sweetalert2/ngx-sweetalert2";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'pdf-viewer';
+  title = "pdf-viewer";
+  pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
+
+  @ViewChild("showInfoSwal", null) private showInfoSwal: SwalComponent;
+
+  openInfo() {
+    console.log("ga");
+
+    // this.showInfoSwal.show();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+// import { SweetAlert2Module } from "node_modules/sweetalert2";
+import { SweetAlert2Module } from "@sweetalert2/ngx-sweetalert2";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { PdfViewerModule } from "ng2-pdf-viewer";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SweetAlert2Module.forRoot(),
+    PdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
